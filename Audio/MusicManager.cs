910ec1560184210ed4ff13b4f2f35694ed595aab
Crypticis <coulton2000m@gmail.com>
2022-11-10 01:18:23 -0500

    Audio initial commit

---
+++ Audio/MusicManager.cs
@@ -0,0 +1,108 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+	public static MusicManager instance;
+
+	private AudioMixerGroup mixerGroup;
+
+	public Sound[] sounds;
+	public Sound[] battleMusic;
+
+	void Awake()
+	{
+		instance = this;
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+
+			s.source.outputAudioMixerGroup = s.mixerGroup;
+		}
+
+		foreach (Sound s in battleMusic)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+
+			s.source.outputAudioMixerGroup = s.mixerGroup;
+		}
+	}
+
+    public void Play(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Play();
+	}
+
+	public void PlayRandomMusic()
+	{
+		Sound s = sounds[UnityEngine.Random.Range(0, sounds.Length)];
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Play();
+		Invoker.InvokeDelayed(PlayRandomMusic, s.source.clip.length);
+		//Invoke("PlayRandomMusic", s.source.clip.length * Time.unscaledDeltaTime);
+	}
+
+	public void Stop(string sound)
+    {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+        s.source.Stop();
+    }
+
+	public void PlayRandomBattleMusic()
+	{
+		Sound s = battleMusic[UnityEngine.Random.Range(0, battleMusic.Length)];
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Play();
+		Invoke("PlayRandomBattleMusic", s.source.clip.length);
+	}
+
+	public void StopAll(Array array)
+	{
+        for (int i = 0; i < array.Length; i++)
+        {
+			sounds[i].source.Stop();
+		}
+	}
+}
